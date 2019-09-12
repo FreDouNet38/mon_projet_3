d4874916c821ace87032d5de0b2c6887415764b1
FreDouNet <frederic.berto38@gmail.com>
2019-09-12 14:51:29 +0200

    Added pycache to the .gitignore

--- classes.py
+++ classes.py
@@ -9,3 +9,38 @@ class Map:
 	def __init__(self,fichier):
 		self.fichier = fichier
 		self.structure = 0
+
+	def generer(self):
+		with open(self.fichier, "r") as fichier:
+			structure_niveau = []
+			for ligne in fichier:
+				ligne_niveau = []
+				for sprite in ligne:
+					if sprite != '\n':
+						ligne_niveau.append(sprite)
+				structure_niveau.append(ligne_niveau)
+			self.structure = structure_niveau
+
+	def afficher(self, fenetre):
+		mur = pygame.image.load(image_wall).convert_alpha()
+		sol = pygame.image.load(image_floor).convert_alpha()
+		gardien = pygame.image.load(image_guard).convert_alpha()
+
+		num_ligne = 0
+		for ligne in self.structure:
+			num_case = 0
+			for sprite in ligne:
+				x = num_case * taille_sprite
+				y = num_ligne * taille_sprite
+				if sprite == 'm':
+					fenetre.blit(mur, (x, y))
+				elif sprite == '0':
+					fenetre.blit(sol, (x, y))
+				elif sprite == 'f':
+					fenetre.blit(gardien, (x, y))
+				num_case += 1
+			num_ligne += 1
+
+
+
+
--- labyrinthe.py
+++ labyrinthe.py
@@ -6,14 +6,25 @@
 import pygame
 from pygame.locals import *
 from constantes import *
-form classes import *
+from classes import *
 
 pygame.init()
 
 #Ouverture de la fenêtre
 fenetre = pygame.display.set_mode((cote_fenetre, cote_fenetre))
 
-#Affichage du titre
 pygame.display.set_caption(titre_fenetre)
+
+continuer = 1
+while continuer:
+	pygame.time.Clock().tick(30)
+	niveau = Map('niveau.txt')
+	niveau.generer()
+	niveau.afficher(fenetre)
+	pygame.display.flip()
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			continuer = 0
 
 
